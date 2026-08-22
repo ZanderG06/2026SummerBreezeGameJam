a24f8143855e9c5a9a3d495ad7b791b2f433933c
Zander Goodwin <zanderngoodwin@gmail.com>
2026-08-22 00:11:59 -0300

    Scrolling complete
    fml

---
+++ Assets/Scripts/EnemyLogic.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyLogic : MonoBehaviour
+{
+    public List<Transform> startPoint;
+    public float moveSpeed;
+
+    private Rigidbody rb;
+    private List<Vector3> waypoints = new List<Vector3>();
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+
+        foreach (Transform childTransform in this.transform) waypoints.Add(childTransform.position);
+
+        int startingLocation = Random.Range(0, startPoint.Count);
+
+        transform.position = startPoint[startingLocation].position;
+
+        StartCoroutine(MoveBetweenWaypoints());
+    }
+
+    IEnumerator MoveBetweenWaypoints()
+    {
+        while (true)
+        {
+            for (int i = 0; i < waypoints.Count; i++)
+            {
+                while (Vector3.Distance(rb.position, waypoints[i]) > .001f)
+                {
+                    rb.MovePosition(Vector3.MoveTowards(rb.position, waypoints[i], moveSpeed * Time.deltaTime));
+
+                    yield return null;
+                }
+                yield return new WaitForSecondsRealtime(1f);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    private float scrollSpeed = 0.25f;
+    private float minY = -6f;
+    private float maxY = 6f;
+
+    private void Update()
+    {
+        float scroll = Mouse.current.scroll.ReadValue().y;
+
+        transform.Translate(0, scroll * scrollSpeed, 0);
+
+        if (transform.position.y < minY) transform.position = new Vector3(transform.position.x, minY, transform.position.z);
+        else if (transform.position.y > maxY) transform.position = new Vector3(transform.position.x, maxY, transform.position.z);
+    }
+}
